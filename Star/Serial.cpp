2544d54b4e6b3b2f5fa05d11759ad85b2a219414
Frank Hliva <frank@hliva.net>
2021-04-06 21:58:27 +0200

    main

--- Star/Serial.cpp
+++ Star/Serial.cpp
@@ -1,172 +1,175 @@
 #include "Serial.h"
 
-Serial::Serial(std::string port, DCB options) : commState({0})
+namespace Star
 {
-	constexpr auto prefix = "\\\\.\\";
-	auto portName = port.rfind(prefix, 0) == 0 ? port : (prefix + port);
-	this->portHandle = CreateFileA(static_cast<LPCSTR>(portName.c_str()),
-		GENERIC_READ | GENERIC_WRITE,
-		0,
-		nullptr,
-		OPEN_EXISTING,
-		FILE_ATTRIBUTE_NORMAL,
-		nullptr
-	);
-	isAvailable = this->portHandle != INVALID_HANDLE_VALUE;
-	if (isAvailable)
+	Serial::Serial(std::string port, DCB options) : commState({ 0 })
 	{
-		DCB commState = {0};
-		if (GetCommState(portHandle, &commState))
+		constexpr auto prefix = "\\\\.\\";
+		auto portName = port.rfind(prefix, 0) == 0 ? port : (prefix + port);
+		this->portHandle = CreateFileA(static_cast<LPCSTR>(portName.c_str()),
+			GENERIC_READ | GENERIC_WRITE,
+			0,
+			nullptr,
+			OPEN_EXISTING,
+			FILE_ATTRIBUTE_NORMAL,
+			nullptr
+		);
+		isAvailable = this->portHandle != INVALID_HANDLE_VALUE;
+		if (isAvailable)
 		{
-			if (options.ByteSize == 0) {
-				options.ByteSize = 8;
-			}
-			commState = options;
-			if (SetCommState(portHandle, &commState))
+			DCB commState = { 0 };
+			if (GetCommState(portHandle, &commState))
 			{
-				isAvailable = true;
-				PurgeComm(portHandle, PURGE_RXCLEAR | PURGE_TXCLEAR);
+				if (options.ByteSize == 0) {
+					options.ByteSize = 8;
+				}
+				commState = options;
+				if (SetCommState(portHandle, &commState))
+				{
+					isAvailable = true;
+					PurgeComm(portHandle, PURGE_RXCLEAR | PURGE_TXCLEAR);
+				}
+				else
+				{
+					console.Log(Errors::SystemError::LastError());
+					throw Errors::Error();
+				}
 			}
 			else
 			{
-				console.Log(Errors::SystemError::LastError());
+				isAvailable = false;
 				throw Errors::Error();
 			}
 		}
 		else
 		{
-			isAvailable = false;
 			throw Errors::Error();
 		}
 	}
-	else
+
+	Serial::Serial(std::string port, DWORD baudRate) :
+		Serial(port, {
+			.BaudRate = baudRate,
+			.fDtrControl = DTR_CONTROL_ENABLE,
+			.fRtsControl = RTS_CONTROL_DISABLE,
+			.ByteSize = 8,
+			.Parity = NOPARITY,
+			.StopBits = ONESTOPBIT
+			})
 	{
-		throw Errors::Error();
 	}
-}
 
-Serial::Serial(std::string port, DWORD baudRate) :
-	Serial(port, {
-		.BaudRate = baudRate,
-		.fDtrControl = DTR_CONTROL_ENABLE,
-		.fRtsControl = RTS_CONTROL_DISABLE,
-		.ByteSize = 8,
-		.Parity = NOPARITY,
-		.StopBits = ONESTOPBIT
-	})
-{
-}
-
-Serial::Serial(std::string port) :
-	Serial(port, CBR_9600)
-{
-}
-
-Serial::~Serial()
-{
-	Close();
-}
-
-DCB Serial::CommState() const
-{
-	return commState;
-}
-
-bool Serial::IsAvailable() const
-{
-	return isAvailable;
-}
-
-DWORD Serial::Write(const char* buffer, const DWORD size)
-{
-	DWORD writtenBytes;
-	if (WriteFile(portHandle, buffer, size, &writtenBytes, nullptr)) {
-		return writtenBytes;
-	}
-	else
+	Serial::Serial(std::string port) :
+		Serial(port, CBR_9600)
 	{
-		return Write(buffer, size);
 	}
-}
 
-DWORD Serial::Read(char* buffer, const DWORD size) const
-{
-	DWORD readedBytes;
-	if (ReadFile(portHandle, buffer, size, &readedBytes, nullptr)) {
-		return readedBytes;
-	}
-	else
+	Serial::~Serial()
 	{
-		throw Errors::Error();
+		Close();
 	}
-}
 
-DWORD Serial::Print(const std::string string)
-{
-	return Write(string.c_str(), string.size());
-}
-
-DWORD Serial::PrintLine(const std::string string)
-{
-	return Print(string + '\n');
-}
-
-std::optional<char> Serial::Read() const
-{
-	char buffer;
-	DWORD readedBytes;
-	if (Read(&buffer, static_cast<DWORD>(1)) == 1)
+	DCB Serial::CommState() const
 	{
-		return buffer;
+		return commState;
 	}
-	else
-	{
-		return std::nullopt;
-	}
-}
 
-std::string Serial::ReadLine(const size_t bufferSize) const
-{
-	char* buffer = new char[bufferSize + 1];
-	char* pointer = buffer;
-	DWORD readedBytes, length = 0;
-	auto line = std::string();
-	while (true) {
-		auto readedBytes = Read(pointer, 1);
-		if (*pointer == '\r' || *pointer == '\n' || readedBytes == 0)
-		{
-			line.append(buffer, length);
-			pointer = nullptr;
-			delete[] buffer;
-			return line;
+	bool Serial::IsAvailable() const
+	{
+		return isAvailable;
+	}
+
+	DWORD Serial::Write(const char* buffer, const DWORD size)
+	{
+		DWORD writtenBytes;
+		if (WriteFile(portHandle, buffer, size, &writtenBytes, nullptr)) {
+			return writtenBytes;
 		}
 		else
 		{
-			if (length == bufferSize)
+			return Write(buffer, size);
+		}
+	}
+
+	DWORD Serial::Read(char* buffer, const DWORD size) const
+	{
+		DWORD readedBytes;
+		if (ReadFile(portHandle, buffer, size, &readedBytes, nullptr)) {
+			return readedBytes;
+		}
+		else
+		{
+			throw Errors::Error();
+		}
+	}
+
+	DWORD Serial::Print(const std::string string)
+	{
+		return Write(string.c_str(), string.size());
+	}
+
+	DWORD Serial::PrintLine(const std::string string)
+	{
+		return Print(string + '\n');
+	}
+
+	std::optional<char> Serial::Read() const
+	{
+		char buffer;
+		DWORD readedBytes;
+		if (Read(&buffer, static_cast<DWORD>(1)) == 1)
+		{
+			return buffer;
+		}
+		else
+		{
+			return std::nullopt;
+		}
+	}
+
+	std::string Serial::ReadLine(const size_t bufferSize) const
+	{
+		char* buffer = new char[bufferSize + 1];
+		char* pointer = buffer;
+		DWORD readedBytes, length = 0;
+		auto line = std::string();
+		while (true) {
+			auto readedBytes = Read(pointer, 1);
+			if (*pointer == '\r' || *pointer == '\n' || readedBytes == 0)
 			{
-				line.append(buffer, length + 1);
-				length = 0;
-				pointer = buffer;
+				line.append(buffer, length);
+				pointer = nullptr;
+				delete[] buffer;
+				return line;
 			}
 			else
 			{
-				pointer++;
-				length++;
+				if (length == bufferSize)
+				{
+					line.append(buffer, length + 1);
+					length = 0;
+					pointer = buffer;
+				}
+				else
+				{
+					pointer++;
+					length++;
+				}
 			}
 		}
 	}
-}
 
-std::string Serial::ReadLine() const
-{
-	return ReadLine(1024);
-}
-
-void Serial::Close()
-{
-	if (this->isAvailable)
+	std::string Serial::ReadLine() const
 	{
-		this->isAvailable = false;
-		CloseHandle(this->portHandle);
+		return ReadLine(1024);
+	}
+
+	void Serial::Close()
+	{
+		if (this->isAvailable)
+		{
+			this->isAvailable = false;
+			CloseHandle(this->portHandle);
+		}
 	}
 }
--- Star/Serial.h
+++ Star/Serial.h
@@ -6,27 +6,30 @@
 #include "Error.h"
 #include "Log.h"
 
-using namespace Star;
-
-class Serial
+namespace Star
 {
-private:
-	DCB commState;
-	HANDLE portHandle;
-	bool isAvailable;
-public:
-	Serial(std::string port, DCB options);
-	Serial(std::string port, DWORD baudRate);
-	Serial(std::string port);
-	~Serial();
-	DCB CommState() const;
-	bool IsAvailable() const;
-	DWORD Write(const char* buffer, const DWORD size);
-	DWORD Read(char* buffer, const DWORD size) const;
-	DWORD Print(const std::string string);
-	DWORD PrintLine(const std::string string);
-	std::optional<char> Read() const;
-	std::string ReadLine(const size_t bufferSize) const;
-	std::string ReadLine() const;
-	void Close();
-};
+
+	class Serial
+	{
+	private:
+		DCB commState;
+		HANDLE portHandle;
+		bool isAvailable;
+	public:
+		Serial(std::string port, DCB options);
+		Serial(std::string port, DWORD baudRate);
+		Serial(std::string port);
+		~Serial();
+		DCB CommState() const;
+		bool IsAvailable() const;
+		DWORD Write(const char* buffer, const DWORD size);
+		DWORD Read(char* buffer, const DWORD size) const;
+		DWORD Print(const std::string string);
+		DWORD PrintLine(const std::string string);
+		std::optional<char> Read() const;
+		std::string ReadLine(const size_t bufferSize) const;
+		std::string ReadLine() const;
+		void Close();
+	};
+
+}
